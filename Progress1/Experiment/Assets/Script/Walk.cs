6600c19da3628f9906d786cd83acd9a2f3b63743
Lester.M <102751769+Nyctophileeee@users.noreply.github.com>
2023-06-26 12:11:25 +0800

    last update

---
+++ Progress1/Experiment/Assets/Script/Walk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm a ball.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
