6bf3f9c2fefc8901ff726dddc5c1192c29ea0773
Lester.M <102751769+Nyctophileeee@users.noreply.github.com>
2023-06-30 16:23:46 +0800

    move

--- Progress2/DeliveryDriver/Assets/Script/Driver.cs
+++ Progress2/DeliveryDriver/Assets/Script/Driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = -0.45f;
+    float moveSpeed = 0.03f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
